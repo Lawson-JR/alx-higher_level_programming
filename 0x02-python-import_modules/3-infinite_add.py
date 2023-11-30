75835f2a29c50d3956dbecf367a641d868b36d99
Lawson-JR <lawson5n2010@gmail.com>
2023-11-30 13:21:36 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+def infinite_add():
+    arguments = sys.argv[1:]
+    result = 0
+
+    for arg in arguments:
+        result += int(arg)
+
+    print(result)
+
+infinite_add()
