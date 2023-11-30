b3c3717180a468384ae528a66bfee0d805458777
Lawson-JR <lawson5n2010@gmail.com>
2023-11-30 12:45:01 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def print_arguments():
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+
+    print(f"Number of argument(s): {num_arguments}")
+
+    if num_arguments == 0:
+        print(".")
+    else:
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
+
+print_arguments()
