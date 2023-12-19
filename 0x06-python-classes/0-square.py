92d0088ea068c6ceb92a0d2a5f1a2769c05b6ede
Lawson-JR <lawson5n2010@gmail.com>
2023-12-19 08:47:48 -0800

    #0 task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,6 @@
 
 class Square:
     def __init__(self):
-        # Constructor method that is called when a new instance of Square is created
+        """Constructor method that is called when a new instance of Square is created"""
         pass
 
