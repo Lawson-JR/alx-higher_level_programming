b6438617a68b83134f23832f46795fea0981c220
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2024-02-15 16:40:09 +0100

    Create 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+
+SELECT User, Host, Grant_priv, Create_priv, Insert_priv, Update_priv, Delete_priv, Select_priv
+FROM mysql.user
+WHERE User IN ('user_0d_1', 'user_0d_2')
+  AND Host = 'localhost';
