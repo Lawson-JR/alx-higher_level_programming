e2b42e8a8e1be07c080cde1512dbd71bce0ce2ec
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2024-03-11 20:53:32 +0100

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+(() => {
+    const numArgs = process.argv.length - 2;
+
+    if (numArgs === ) {
+        console.log("No argument");
+    } else if (numArgs === 1) {
+        console.log("Argument found");
+    } else {
+        console.log("Arguments found");
+    }
+})();
