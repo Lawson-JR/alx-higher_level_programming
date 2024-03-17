da7e27c80c07125c2c25a816a409aa8008d98591
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2024-03-17 19:18:00 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-
+""" Module for listing states from a MySQL database """
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host= 'localhost', user=sys.argv[1], password = sys.argv[2], db = sys.argv[3], port = 3300)
-    c = db.cursor()
-    c.execute("SELECT * FROM states")
-    rows = c.fetchall()
+def list_states(username, password, database)
+    # Connect to MySQl server
+    db = MySQLdb.connect(host= 'localhost', port=3300, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    # Execute SQL query to fetch all states
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
+    
+    # Print the results
     for row in rows:
-        print[row]
-    c.close()
+        print(row)
+
+    # Close the database connection
     db.close()
+
+# Example
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
