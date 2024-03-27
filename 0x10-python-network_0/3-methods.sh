dc238b473fd6ae8b4a7aec470d6cea6dca401200
Lawson-JR <lawson5n2010@gmail.com>
2024-03-27 13:42:49 -0700

    Update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
 
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+#!/bin/bash
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
+
